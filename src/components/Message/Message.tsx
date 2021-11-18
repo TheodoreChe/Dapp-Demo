c51124fefb5dd783d223490163a56afa815371dc
theodoreche <2226390@gmail.com>
2021-11-18 19:53:10 +0100

    ✨ add warning message component

---
+++ src/components/Message/Message.tsx
@@ -0,0 +1,23 @@
+import { FC, ReactElement } from 'react'
+
+type Props = {
+  children: ReactElement
+}
+
+export const Message: FC<Props> = ({ children }) => {
+  return <div>{children}</div>
+}
+
+export const MetamaskMessage: FC = () => {
+  return (
+    <Message>
+      <>
+        You need to{' '}
+        <a target="_blank" href={`https://metamask.io/download.html`}>
+          install Metamask
+        </a>
+        , a virtual Ethereum wallet, in your browser.
+      </>
+    </Message>
+  )
+}
